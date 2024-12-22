f373b2b90292810be2db886211fbf9a6901cb118
arthraw <arthursallesandrade@gmail.com>
2024-12-22 02:00:00 -0300

    fix: str imports added to create uuid

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Support\Str;
 
 
 class User extends Authenticatable
