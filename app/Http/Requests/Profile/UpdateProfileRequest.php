e83f380400ca94607744f51efad53f0fbcaa8f11
arthraw <arthursallesandrade@gmail.com>
2024-12-25 15:41:22 -0300

    fix: moved to profile dir

--- app/Http/Requests/Profile/UpdateProfileRequest.php
+++ app/Http/Requests/Profile/UpdateProfileRequest.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Http\Requests\User;
+namespace App\Http\Requests\Profile;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateUserRequest extends FormRequest
+class UpdateProfileRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
