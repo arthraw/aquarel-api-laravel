2a6e5399ce2e5bfabfd1e354b807df6e69d7a098
arthraw <arthursallesandrade@gmail.com>
2024-12-31 18:17:03 -0300

    tests: route /profile -> headers impl in profile tests

--- tests/Feature/Http/Controller/ProfileControllerTest.php
+++ tests/Feature/Http/Controller/ProfileControllerTest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace Http\Controller;
+namespace Tests\Feature\Http\Controller;
 
+use App\Models\Profile;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
@@ -13,9 +14,12 @@ class ProfileControllerTest extends TestCase
     {
         $fakeProfile = [
             'profile_id' => '34486aa4-55d2-4dbe-ab0d-0d2fffaf441a',
-            'email' => 'etevaldo@example.com'
+            'email' => 'etevaldo@example.com',
         ];
-        $response = $this->patchJson(route('profile.update'), $fakeProfile);
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->patchJson(route('profile.update'), $fakeProfile, $header);
         $response
             ->assertStatus(200)
             ->assertJson([
@@ -27,9 +31,12 @@ class ProfileControllerTest extends TestCase
     {
         $fakeProfile = [
             'profile_id' => '1',
-            'email' => 'etevaldo@example.com'
+            'email' => 'etevaldo@example.com',
         ];
-        $response = $this->patchJson(route('profile.update'), $fakeProfile);
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->patchJson(route('profile.update'), $fakeProfile, $header);
         $response
             ->assertStatus(404)
             ->assertJson([
@@ -42,9 +49,12 @@ class ProfileControllerTest extends TestCase
         $fakeProfile = [
             'profile_id' => '34486aa4-55d2-4dbe-ab0d-0d2fffaf441a',
             'password' => 'Senhasegura123',
-            'password_check' => 'teste1234'
+            'password_check' => 'teste1234',
         ];
-        $response = $this->patchJson(route('profile.update'), $fakeProfile);
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->patchJson(route('profile.update'), $fakeProfile, $header);
         $response
             ->assertStatus(200)
             ->assertJson([
@@ -58,7 +68,10 @@ class ProfileControllerTest extends TestCase
             'profile_id' => '34486aa4-55d2-4dbe-ab0d-0d2fffaf441a',
             'password' => 'Senhasegura123',
         ];
-        $response = $this->patchJson(route('profile.update'), $fakeProfile);
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->patchJson(route('profile.update'), $fakeProfile, $header);
         $response
             ->assertStatus(404)
             ->assertJson([
@@ -71,9 +84,12 @@ class ProfileControllerTest extends TestCase
         $fakeProfile = [
             'profile_id' => '34486aa4-55d2-4dbe-ab0d-0d2fffaf441a',
             'name' => 'Ete Valdo',
-            'bio' => '🌸 Oiê! Bem-vindos ao meu universo artístico! Sou uma artista apaixonada por desenhar e pintar animais. Aqui, cada traço é uma jornada pela fauna, e cada cor conta uma história selvagem. Vamos explorar juntos esse reino encantado onde a imaginação e a natureza se encontram! 🐾✨'
+            'bio' => '🌸 Oiê! Bem-vindos ao meu universo artístico! Sou uma artista apaixonada por desenhar e pintar animais. Aqui, cada traço é uma jornada pela fauna, e cada cor conta uma história selvagem. Vamos explorar juntos esse reino encantado onde a imaginação e a natureza se encontram! 🐾✨',
         ];
-        $response = $this->patchJson(route('profile.update'), $fakeProfile);
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->patchJson(route('profile.update'), $fakeProfile, $header);
         $response
             ->assertStatus(200)
             ->assertJson([
@@ -86,9 +102,24 @@ class ProfileControllerTest extends TestCase
         $fakeProfile = [
             'profile_id' => '34486aa4-55d2-4dbe-ab0d-0d2fffaf441a',
         ];
-        $response = $this->postJson(route('profile.my'), $fakeProfile);
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->postJson(route('profile.my'), $fakeProfile, $header);
         $response
             ->assertStatus(200);
     }
 
+    public function testGetProfileWithNoExistentProfileId()
+    {
+        $fakeProfile = [
+            'profile_id' => '1',
+        ];
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->postJson(route('profile.my'), $fakeProfile, $header);
+        $response
+            ->assertStatus(404);
+    }
 }
