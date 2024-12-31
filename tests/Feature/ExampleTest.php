be207c2aeb8ff4c3db699e4e786e32d2adb117a4
arthraw <arthursallesandrade@gmail.com>
2024-12-31 18:11:04 -0300

    tests: route /v1

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -12,8 +12,10 @@ class ExampleTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        $response = $this->get('/');
-
+        $header = [
+            'token' => env('API_TOKEN')
+        ];
+        $response = $this->get('v1/', $header);
         $response->assertStatus(200);
     }
 }
